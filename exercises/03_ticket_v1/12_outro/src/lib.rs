475f9670acfa6d14bb6dc43a04c41b3f1dfbf1de
Siu Fong <xprongsf@gmail.com>
2025-08-02 12:15:54 +0800

    Heap and memory layout (2/Aug/2025)

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,67 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Order {
+            product_name: Order::sanitize_product_name(product_name),
+            quantity: Order::assert_value_greater_than_zero(quantity),
+            unit_price: Order::assert_value_greater_than_zero(unit_price)
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = Order::sanitize_product_name(product_name);
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = unit_price;
+    }
+
+    fn sanitize_product_name(product_name: String) -> String{
+        if product_name.is_empty() {
+            panic!("Product name can't be empty!")
+        }
+
+        if product_name.len() > 300 {
+            panic!("Product name can't be longer than 300 bytes!")
+        }
+
+        product_name
+    }
+
+    fn assert_value_greater_than_zero(value: u32) -> u32{
+        if value == 0 {
+            panic!("Value must be greater than 0!")
+        }
+
+        value
+    }
+}
