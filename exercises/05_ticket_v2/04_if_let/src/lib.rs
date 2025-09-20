8cdd84bfc4946f21f850123dfcef772dcadf19fa
Siu Fong <xprongsf@gmail.com>
2025-09-20 22:37:28 +0800

    Enum, matching, variant, tuple, if/let, Option, Result [15/Sept/2025]

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,15 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        let Shape::Circle { radius } = self else {
+            panic!("Only circle can has radius");
+        };
+        *radius
+        // if let Shape::Circle{ radius } = self {
+        //     *radius
+        // } else {
+        //     panic!("Only circle can has radius")
+        // }
     }
 }
 
--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,16 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description.clone(), status.clone()) {
+        Ok(tic) => tic,
+        Err(msg) => {
+            let is_title_error = msg.contains("Title");
+            if is_title_error {
+                panic!("{}", msg);
+            }
+            Ticket::new(title, "Description not provided".into(), status).unwrap()
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
